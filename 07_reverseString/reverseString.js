ef4ee3e447e03bb7b40fdf9c339fa48c43a27811
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-19 07:38:55 +0800

    07_reverseString : Use the split, reverse, and join to return a reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  return word.split("").reverse().join("");
 };
+
+console.log(reverseString("word"));
 
 // Do not edit below this line
 module.exports = reverseString;
