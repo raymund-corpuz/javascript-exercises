84a247e686cd8492f942832aa15b125ce3d13aaf
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-19 09:20:06 +0800

    08_removeFromArray : Create a function that accepts parameters then storing it in a variable then make a loop, check if the item includes something that is pass as argument then return the result

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...toRemove) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (!toRemove.includes(array[i])) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
