af83feee0055572002b98b35d12f03efd28d4384
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-19 10:54:31 +0800

    10_leapYears:a year is a leap Year if it is divisible by 4 and divisible by 400 then return true.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  //a year is a leap Year if it is divisible by 4 and divisible by 400
 
+  if (year % 4 !== 0 && year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 };
-
+console.log(leapYears(2012));
 // Do not edit below this line
 module.exports = leapYears;
