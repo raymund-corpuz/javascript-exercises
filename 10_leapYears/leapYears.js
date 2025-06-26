1b447f7008e6ece9f7d5fd487f1670a8fa3843f6
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-26 08:21:11 +0800

    11_tempConversion : Convert Celcius -32 to Fahrenheit + 32

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -7,6 +7,6 @@ const leapYears = function (year) {
     return true;
   }
 };
-console.log(leapYears(2012));
+console.log(leapYears(100));
 // Do not edit below this line
 module.exports = leapYears;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  return temp - 32;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return Math.floor(temp + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
