480dc7d5d4b662902455499ed3ef294a75b7ccd0
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-26 08:51:36 +0800

    13_palindrome : Create a function that check if it is a palindrome or not then return true if palindrome else false

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (...word) {
+  const original = word;
 
+  const palindrome = word.reverse();
+  if (original == palindrome) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
