acf28e71fb046eed2611f58d26ad696eb91b12e0
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-06-26 08:42:58 +0800

    12_calculator : Create a function Add, Sub, Multiply, Sum and factorial.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return Number(num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  const result = num.reduce((acc, curr) => {
+    return acc + curr;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (num1, num2) {
+  return num1 * num2;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0 || num === 1) return 1;
+  const arr = Array.from({ length: num }, (_, i) => i + 1);
+  const result = arr.reduce((acc, curr) => acc * curr, 1);
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
